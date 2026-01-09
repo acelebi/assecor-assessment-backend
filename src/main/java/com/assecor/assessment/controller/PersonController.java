420dce4739e669996ac99124d520ff670b90f0d7
acelebi <abcelebi@gmail.com>
2026-01-09 15:23:02 +0100

    Implement PersonController for managing persons

---
+++ src/main/java/com/assecor/assessment/controller/PersonController.java
@@ -0,0 +1,42 @@
+package com.assecor.assessment.controller;
+
+import com.assecor.assessment.model.Person;
+import com.assecor.assessment.service.PersonService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable Long id) {
+        return personService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/color/{color}")
+    public List<Person> getPersonsByColor(@PathVariable String color) {
+        return personService.findByColor(color);
+    }
+
+    // Bonus: POST to add new person
+    @PostMapping
+    public Person addPerson(@RequestBody Person person) {
+        return personService.save(person);
+    }
+}
